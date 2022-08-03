5cd9398fa0aa52b625102c9e3dd1752269d45b4f
rodrigo <rodrigovmoraes@gmail.com>
2022-08-03 11:13:37 -0300

    testPostNewRecipeForm test was fixed

--- src/test/java/guru/springframework/controllers/RecipeControllerTest.java
+++ src/test/java/guru/springframework/controllers/RecipeControllerTest.java
@@ -92,6 +92,7 @@ public class RecipeControllerTest {
                 .contentType(MediaType.APPLICATION_FORM_URLENCODED)
                 .param("id", "")
                 .param("description", "some string")
+                .param("directions", "Lorem ipsum dolor sit amet, consectetur adipiscing elit.")
         )
                 .andExpect(status().is3xxRedirection())
                 .andExpect(view().name("redirect:/recipe/2/show"));
